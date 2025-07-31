d1854d35bbb2f39d46e486be5f8b22537a7deec7
Pedro Henrique <pedrohenriquesj.pro@gmail.com>
2025-07-31 15:51:08 -0300

    Implement code changes to enhance functionality and improve performance

--- src/shared/modules/database/database.module.ts
+++ src/shared/modules/database/database.module.ts
@@ -1,7 +1,8 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { ConfigModule, ConfigService } from "@nestjs/config";
-import { getDataSource } from "./data-source";
+import { DatabaseService } from "./database.service";
+import { ConfigurationEntity } from "./entities/configurarion.entity";
 
 @Module({
   imports: [
@@ -11,15 +12,18 @@ import { getDataSource } from "./data-source";
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: async (config: ConfigService) => {
-        const dataSource = await getDataSource(config);
-
-        return {
-          ...dataSource.options,
-          keepConnectionAlive: true,
-        };
-      },
+      useFactory: async (config: ConfigService) => ({
+        type: "postgres",
+        host: config.get<string>("DB_HOST"),
+        port: config.get<number>("DB_PORT"),
+        username: config.get<string>("DB_USERNAME"),
+        password: config.get<string>("DB_PASSWORD"),
+        database: config.get<string>("DB_NAME"),
+        entities: [__dirname + "/../**/*.entity{.ts,.js}"],
+      }),
     }),
+    TypeOrmModule.forFeature([ConfigurationEntity]),
   ],
+  providers: [DatabaseService],
 })
 export class DatabaseModule {}
