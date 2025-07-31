66a82257fabf18606c9cfccafb950a2351cf7dcc
Pedro Henrique <pedrohenriquesj.pro@gmail.com>
2025-07-31 16:33:48 -0300

    refactor: streamline database connection configuration for improved clarity

--- src/shared/modules/database/database.module.ts
+++ src/shared/modules/database/database.module.ts
@@ -12,27 +12,25 @@ import { ConfigurationEntity } from "./entities/configurarion.entity";
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: async (config: ConfigService) =>
-        process.env.NODE_ENV === "production"
-          ? {
-              type: "postgres",
-              url: config.get<string>("DATABASE_URL"),
-              ssl: {
-                rejectUnauthorized: false, // necessário para o Supabase
-              },
-            }
-          : {
-              type: "postgres",
-              host: config.get<string>("DB_HOST"),
-              port: config.get<number>("DB_PORT"),
-              username: config.get<string>("DB_USERNAME"),
-              password: config.get<string>("DB_PASSWORD"),
-              database: config.get<string>("DB_NAME"),
-              entities: [__dirname + "/../**/*.entity{.ts,.js}"],
-            },
+      useFactory: async (config: ConfigService) => ({
+        ssl: {
+          rejectUnauthorized: false, // necessário para o Supabase
+        },
+        type: "postgres",
+        host: config.get<string>("DB_HOST"),
+        port: config.get<number>("DB_PORT"),
+        username: config.get<string>("DB_USERNAME"),
+        password: config.get<string>("DB_PASSWORD"),
+        database: config.get<string>("DB_NAME"),
+        entities: [__dirname + "/../**/*.entity{.ts,.js}"],
+      }),
     }),
     TypeOrmModule.forFeature([ConfigurationEntity]),
   ],
   providers: [DatabaseService],
 })
 export class DatabaseModule {}
+
+// postgresql://postgres:QLJIgKNyWEibGpBPjBYeQzqNPTBUPYJR@mainline.proxy.rlwy.net:27130/railway
+
+// postgresql://postgres:Vs6OHENHpsD0yAte@db.wchteqronqwfvatpitzl.supabase.co:5432/postgres
